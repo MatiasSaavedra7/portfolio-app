23c9201c1d715b60bc0c40eafe40f880c4489ba6
Matias Saavedra <matiassaavedra1995@gmail.com>
2025-07-08 16:01:14 -0300

    Actualizo el footer

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -4,7 +4,7 @@ export default function Footer() {
       <div className="container px-4 mx-auto">
         <div className="text-center text-sm text-muted-foreground">
           {/* <p>&copy; 2024 Matias Saavedra. Todos los derechos reservados.</p> */}
-          <p>Made with ❤️ by <a href="https://github.com/MatiasSaavedra7" target="_blank" rel="noopener noreferrer" className="text-blue-600 hover:underline dark:text-blue-400 transition-colors">Matias Saavedra</a></p>
+          <p>Hecho con ❤️ por <a href="https://github.com/MatiasSaavedra7" target="_blank" rel="noopener noreferrer" className="text-blue-600 hover:underline dark:text-blue-400 transition-colors">Matias Saavedra</a></p>
         </div>
       </div>
     </footer>
